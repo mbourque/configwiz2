922f49631ac0771100b1fb40575521a13a771d9d
mbourque <michael@reversestartup.org>
2025-03-15 07:12:07 -0400

    Remove Google Analytics script from multiple template files for cleaner code and to avoid unnecessary tracking.

--- templates/configure.php
+++ templates/configure.php
@@ -1,15 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-9Y2TFBLBWH"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'G-9Y2TFBLBWH');
-    </script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ConfigWiz - <?= $category ?></title>
