e790603eb1c814b5e22b6219f9b69db409b290c9
Michael Bourque <mbourque@users.noreply.github.com>
2025-03-29 11:18:54 -0400

    Update footer.php
    Fix last commit

--- templates/includes/footer.php
+++ templates/includes/footer.php
@@ -1,7 +1,7 @@
 <!-- Site Footer -->
 <footer class="site-footer">
     <div class="footer-content">
-        <p>Created by: <a href="mailto:michael@ptcuserboston.org">Michael P. Bourque</a> | <a htef="https://github.com/mbourque/configwiz2/blob/master/release-notes.md" target="_blank">Release Notes</a> | <a href="https://github.com/mbourque/configwiz2" target="_blank"><i class="fa-brands fa-github"></i> GitHub</a></p>
+        <p>Created by: <a href="mailto:michael@ptcuserboston.org">Michael P. Bourque</a> | <a href="https://github.com/mbourque/configwiz2/blob/master/release-notes.md" target="_blank">Release Notes</a> | <a href="https://github.com/mbourque/configwiz2" target="_blank"><i class="fa-brands fa-github"></i> GitHub</a></p>
     </div>
 </footer>
 
