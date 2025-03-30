5f9ad15c00bf3791c720159d9ed5bb7ea23382e6
Michael Bourque <mbourque@users.noreply.github.com>
2025-03-30 07:56:44 -0400

    Update footer.php

--- templates/includes/footer.php
+++ templates/includes/footer.php
@@ -3,7 +3,7 @@
     <div class="footer-content">
         <p>
             Created by: <a href="mailto:michael@ptcuserboston.org">Michael P. Bourque</a> |
-            <a href="https://ptcuserboston.org/creoverse/" target="_blank">Creoverse</a> |
+            <a href="https://ptcuserboston.org/creoverse/" target="_blank">CreoVerse</a> |
             <a href="https://github.com/mbourque/configwiz2/blob/master/release-notes.md" target="_blank">Release Notes</a> |
             <a href="https://github.com/mbourque/configwiz2" target="_blank"><i class="fa-brands fa-github"></i> GitHub</a>
         </p>
