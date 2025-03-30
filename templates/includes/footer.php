386ee284a991d140459a07bedc2d9c5472219491
Michael Bourque <mbourque@users.noreply.github.com>
2025-03-30 07:55:30 -0400

    Update footer.php
    Added link to CreoVerse

--- templates/includes/footer.php
+++ templates/includes/footer.php
@@ -1,10 +1,15 @@
 <!-- Site Footer -->
 <footer class="site-footer">
     <div class="footer-content">
-        <p>Created by: <a href="mailto:michael@ptcuserboston.org">Michael P. Bourque</a> | <a href="https://github.com/mbourque/configwiz2/blob/master/release-notes.md" target="_blank">Release Notes</a> | <a href="https://github.com/mbourque/configwiz2" target="_blank"><i class="fa-brands fa-github"></i> GitHub</a></p>
+        <p>
+            Created by: <a href="mailto:michael@ptcuserboston.org">Michael P. Bourque</a> |
+            <a href="https://ptcuserboston.org/creoverse/" target="_blank">Creoverse</a> |
+            <a href="https://github.com/mbourque/configwiz2/blob/master/release-notes.md" target="_blank">Release Notes</a> |
+            <a href="https://github.com/mbourque/configwiz2" target="_blank"><i class="fa-brands fa-github"></i> GitHub</a>
+        </p>
     </div>
 </footer>
 
 <script src="static/js/script.js?v=<?= time() ?>"></script>
 </body>
-</html> 
+</html>
